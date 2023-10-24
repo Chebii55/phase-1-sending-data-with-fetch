eed15c320313f903fc78ad3d81039c24d59bd540
Sheila Chebii <shillerchier055@gmail.com>
2023-10-24 22:11:33 +0300

    pending

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(uName,uMail){
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${uName}`,
+      email:`${uMail}`,
+    }),
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .catch(function (error) {
+    alert("Bad things! ERROR!");
+    console.log(error.message);
+  })
+  
+}
